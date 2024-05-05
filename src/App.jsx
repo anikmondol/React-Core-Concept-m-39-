86770ea51985a6f2b6959da120bdabad4001b2c8
anikmondol <anikmondol558363@gmil.com>
2024-05-05 14:48:58 +0600

    different way to handle events in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  function handleClick(){
+    alert("button clicked")
+  }
+
+  const handleClick2 = () =>{
+    alert("button clicked2")
+  }
+
+  const addToFive = (num) =>{
+    alert(5 + num);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+      <h2>React Core Concept</h2>
+      {/* <button onclick="handleClick()">Click Me</button> */}
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click2</button>
+      <button onClick={() => {alert("This is a third")}}>Third</button>
+      <button onClick={function handleClick4(){
+        alert("alert by fourth")}}>Fourth</button>
+        {/* vejailla */}
+      <button onClick={() => addToFive(3)}>Add to Five</button>  
     </>
   )
 }
